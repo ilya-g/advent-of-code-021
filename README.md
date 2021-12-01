4a7e18454c21982f50bda28839d793c87bb1ad4e
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2021-12-01 08:40:48 +0300

    Project structure

---
+++ README.md
@@ -0,0 +1,3 @@
+# Advent of Code 2021
+
+Advent of code 2021 solutions in Kotlin 1.6
---
+++ build.gradle.kts
@@ -0,0 +1,32 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.6.0"
+}
+
+group = "org.test"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+sourceSets {
+    main {
+        java.srcDir("src")
+    }
+    test {
+        java.srcDir("test")
+    }
+}
+
+kotlin {
+    sourceSets.all {
+        languageSettings {
+            optIn("kotlin.time.ExperimentalTime")
+//            optIn("kotlin.io.path.ExperimentalPathApi")
+            optIn("kotlin.ExperimentalStdlibApi")
+        }
+    }
+}
+
