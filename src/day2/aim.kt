2929deea5858a0486cdebf03ea3a70c4d4a25a6a
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2021-12-02 08:20:37 +0300

    Task 2

---
+++ src/day2/aim.kt
@@ -0,0 +1,43 @@
+package day2
+
+import kotlin.io.path.Path
+import kotlin.io.path.readLines
+
+enum class Command {
+    forward, down, up
+}
+
+fun main() {
+    val commands = Path("src/day2/input.txt").readLines()
+        .map { it.split(' ').let { (a, b) -> Command.valueOf(a) to b.toInt() } }
+
+    run part1@ {
+        var x = 0
+        var depth = 0
+        for ((command, value) in commands) {
+            when (command) {
+                Command.forward -> x += value
+                Command.down -> depth += value
+                Command.up -> depth -= value
+            }
+        }
+        println("x: $x, depth: $depth; ${x * depth}")
+    }
+
+    run part2@ {
+        var x = 0
+        var depth = 0
+        var aim = 0
+        for ((command, value) in commands) {
+            when (command) {
+                Command.forward -> {
+                    x += value
+                    depth += aim * value
+                }
+                Command.down -> aim += value
+                Command.up -> aim -= value
+            }
+        }
+        println("x: $x, depth: $depth; ${x * depth}")
+    }
+}
