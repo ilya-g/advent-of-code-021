ffd97fb2861d51fc89e267c9639009a8de560c01
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2021-12-26 08:19:18 +0300

    Day 24: find pairs of digits affecting each other from a random solution

--- src/day24/modelNumber.kt
+++ src/day24/modelNumber.kt
@@ -1,7 +1,6 @@
 package day24
 
 import common.readLines
-import java.lang.StringBuilder
 
 enum class OpCode {
     inp,
@@ -20,7 +19,7 @@ fun parseInstr(s: String): Instr =
 
 fun Long.toChars(): String = generateSequence(this) { it / 26 }.takeWhile { it > 0 }.map { 'a' + (it % 26).toInt() }.toList().reversed().joinToString("")
 
-fun execute(input: CharSequence, program: List<Instr>, verbose: Boolean = true): Long {
+fun execute(input: CharSequence, program: List<Instr>, verbose: Boolean = false): Long {
     check(input.all { it in '1'..'9' })
     val regs = Array(4) { 0L }
     fun store(reg: String, value: Long) {
@@ -48,7 +47,7 @@ fun execute(input: CharSequence, program: List<Instr>, verbose: Boolean = true):
     return regs.last()
 }
 
-fun analyzeNullifyingPairs(program: List<Instr>) = buildList {
+fun staticAnalyzeNullifyingPairs(program: List<Instr>) = buildList {
     val posStack = mutableListOf<Int>()
     var pos = -1
     for ((opCode, args) in program) {
@@ -63,6 +62,29 @@ fun analyzeNullifyingPairs(program: List<Instr>) = buildList {
     check(posStack.isEmpty())
 }
 
+fun dynamicAnalyzeNullifyingPairs(program: List<Instr>, base: String): List<Pair<Int, Int>> = buildList {
+    check(execute(base, program) == 0L)
+    val s = StringBuilder()
+    nextPair@ for (p1 in base.indices) {
+        if (this.any { it.second == p1 }) continue
+        for (p2 in p1 + 1 until base.length) {
+            s.clear().append(base)
+            for (v1 in 1..9) {
+                s[p1] = v1.digitToChar()
+                if (s[p1] == base[p1]) continue
+                for (v2 in 1..9) {
+                    s[p2] = v2.digitToChar()
+                    if (execute(s, program) == 0L) {
+                        add(p1 to p2)
+                        continue@nextPair
+                    }
+                }
+            }
+        }
+    }
+    sortBy { it.second }
+}
+
 fun <T, K : Comparable<K>> List<T>.allMinsBy(selector: (T) -> K): List<T> {
     val min = minOf(selector)
     return filter { selector(it) == min }
@@ -73,10 +95,8 @@ fun main() {
     val program = readLines("day24").map(::parseInstr) //.onEach(::println)
 
     // 94815972183576 - one carefully found valid model number
-    val r = execute("94815972183576", program)
+    val r = execute("94815972183576", program, verbose = true)
     check(r == 0L)
-
-    val pairs = analyzeNullifyingPairs(program).also(::println)
 
     fun results(base: String, pair: Pair<Int, Int>) = sequence {
         val s = StringBuilder(base)
@@ -90,12 +110,42 @@ fun main() {
     fun varySolutions(base: String, pair: Pair<Int, Int>) =
         results(base, pair).filter { it.second == 0L }
 
+
+    fun randomSolution(start: String): String {
+        fun Long.digitsBase26(): Int {
+            var p = 0
+            var v1 = this
+            while (v1 > 0) { v1 /= 26; p++ }
+            return p
+        }
+        val allPairs = start.indices.flatMap { p1 -> (p1 + 1 until start.length).map { p2 -> p1 to p2 } }
+        var s = start
+        do {
+            val p = allPairs.random()
+            val min = results(s, p).toList().allMinsBy { it.second.digitsBase26() }
+                .apply { println("$size ${first()} - ${last()}") }
+                .random()
+            s = min.first
+        } while (min.second != 0L)
+        return s
+    }
+
+    val s1 = randomSolution(CharArray(14) { (1..9).random().digitToChar() }.concatToString())
+    println("Random solution: $s1")
+    check(execute(s1, program) == 0L)
+
+    val staticPairs = staticAnalyzeNullifyingPairs(program)
+    val pairs = dynamicAnalyzeNullifyingPairs(program, s1)
+    println("Statically analyzed pairs:  $staticPairs")
+    println("Dynamically analyzed pairs: $pairs")
+
     val start = "1".repeat(14)
 
     val solutions = pairs.fold(listOf(start to 0L)) { stage, pair ->
         stage.flatMap { results(it.first, pair) }.allMinsBy { it.second } //.also { println(it) }
     }.map { it.first }
 
+    println(solutions.size)
     println(solutions.maxOrNull()!!)
     println(solutions.minOrNull()!!)
 
